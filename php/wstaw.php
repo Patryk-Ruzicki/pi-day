57903198a94cda220f4a832686f86bfcb94079a6
Patryk-Ruzicki <arcysanta@gmail.com>
2023-03-08 17:22:04 +0100

    Upload files

---
+++ php/wstaw.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dzień Pi - Wstawianie</title>
+    <style>
+        table {
+            border: 1px solid black;
+        }
+    </style>
+</head>
+<body>
+    <h2>Dzień Pi - rezultaty</h2>
+    <?php
+        $FirstName = $_POST['FirstName'];
+        $LastName = $_POST['LastName']
+        $Class = $_POST['Class'];
+
+        $FirstName = trim($FirstName);
+        $LastName = trim($LastName);
+        $Class = trim($Class);
+
+        $FirstName = addslashes($FirstName);
+        $LastName = addslashes($LastName);
+        $Class = addslashes($Class);
+
+        @ $db = new mysqli('localhost', 'root', '', 'pi_day');
+        if(mysqli_connect_errno()){
+            echo "Błąd połączenia z bazą danych!";
+            exit;
+        }
+
+        $StudentID++;
+        $zapytanie = "insert into ksiazki value ('".$StudentID."', '".$FirstName."','".$LastName."','".$Class."')<br>";
+        echo $zapytanie;
+
+        $wynik = $db -> query($zapytanie);
+        if($wynik){
+            echo $db -> affected_rows." student zapisany do bazy.";
+        }else{
+            echo "Błąd. Uczeń nie został zapisany do bazy.";
+        }
+        $db -> close();
+    ?>
+</body>
+</html>
